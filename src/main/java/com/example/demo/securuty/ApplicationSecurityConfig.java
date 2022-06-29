82f174cbbd23fa33f0e0ea802327a68f1d488c20
Andrii_Barsuk <Andrii_Barsuk@epam.com>
2022-06-29 16:28:16 +0300

    Users_Roles_And_Authorities

---
+++ src/main/java/com/example/demo/securuty/ApplicationSecurityConfig.java
@@ -0,0 +1,74 @@
+package com.example.demo.securuty;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.example.demo.securuty.ApplicationUserPermission.COURSE_WRITE;
+import static com.example.demo.securuty.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTREINEE.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annasmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+//                .roles(STUDENT.name()) //ROLE_STUDENT
+                .authorities(STUDENT.grantedAuthorities())
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMIN.name()) //ROLE_ADMIN
+                .authorities(ADMIN.grantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+//                .roles(ADMINTREINEE.name()) //ROLE_ADMINTREINEE
+                .authorities(ADMINTREINEE.grantedAuthorities())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annasmithUser,
+                lindaUser,
+                tomUser);
+    }
+}
---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,10 @@
+package com.example.demo.student;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+}
